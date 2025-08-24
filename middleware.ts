1a31feabd247e799ef9705b66f35eee8b7ac7118
Gregor Wallner <greg.wallner@gmail.com>
2025-08-25 01:24:10 +0200

    Fix Sitemap & Robots in Middleware

--- middleware.ts
+++ middleware.ts
@@ -76,6 +76,16 @@ export default async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|public).*)",
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - robots.txt (robots file)
+     * - sitemap.xml (sitemap file)
+     * - site.webmanifest (PWA manifest)
+     * - images/icons in public folder
+     */
+    "/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|site.webmanifest|.*\\.png|.*\\.jpg|.*\\.jpeg|.*\\.gif|.*\\.svg|.*\\.ico).*)",
   ],
 }
